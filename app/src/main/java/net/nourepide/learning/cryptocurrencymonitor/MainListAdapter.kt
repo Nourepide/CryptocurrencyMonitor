6aa5db685c401bd52a8c14f94fa8d0b08c73c811
Nourepide <nourepide@gmail.com>
2018-11-01 17:17:25 +0400

    Use ItemCryptocurrencyBinding

--- app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
+++ app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
@@ -1,21 +1,14 @@
 package net.nourepide.learning.cryptocurrencymonitor
 
-import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import net.nourepide.learning.cryptocurrencymonitor.MainListAdapter.MainViewHolder
+import net.nourepide.learning.cryptocurrencymonitor.databinding.ItemCryptocurrencyBinding
 import net.nourepide.learning.cryptocurrencymonitor.entity.Cryptocurrency
 
 class MainListAdapter(private val viewModel: MainViewModel) : RecyclerView.Adapter<MainViewHolder>() {
-    class MainViewHolder(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(data: Cryptocurrency) {
-            binding.setVariable(BR.cryptocurrency, data)
-            binding.executePendingBindings()
-        }
-    }
-
     override fun onCreateViewHolder(viewGroup: ViewGroup, value: Int) = MainViewHolder(getLayout(viewGroup))
 
     override fun getItemCount() = viewModel.data.size
@@ -24,11 +17,17 @@ class MainListAdapter(private val viewModel: MainViewModel) : RecyclerView.Adapt
         viewHolder.bind(viewModel.data[value])
     }
 
-    private fun getLayout(viewGroup: ViewGroup): ViewDataBinding = DataBindingUtil
+    private fun getLayout(viewGroup: ViewGroup) = ItemCryptocurrencyBinding
         .inflate(
             LayoutInflater.from(viewGroup.context),
-            R.layout.item_cryptocurrency,
             viewGroup,
             false
         )
+
+    class MainViewHolder(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Cryptocurrency) {
+            binding.setVariable(BR.cryptocurrency, data)
+            binding.executePendingBindings()
+        }
+    }
 }
