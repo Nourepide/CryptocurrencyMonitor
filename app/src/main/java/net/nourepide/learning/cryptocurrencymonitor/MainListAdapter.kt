208e306232535783f40403fb089f53f2699e1dd6
Nourepide <nourepide@gmail.com>
2018-11-01 13:33:49 +0400

    Reimplement RecyclerView with DataBinding

--- app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
+++ app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
@@ -1,32 +1,32 @@
 package net.nourepide.learning.cryptocurrencymonitor
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import net.nourepide.learning.cryptocurrencymonitor.entity.Cryptocurrency
 
 class MainListAdapter(private val viewModel: MainViewModel) : RecyclerView.Adapter<MainListAdapter.MainViewHolder>() {
     override fun onCreateViewHolder(viewGroup: ViewGroup, value: Int) = MainViewHolder(
-        LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.item_cryptocurrency, viewGroup, false)
+        DataBindingUtil.inflate<ViewDataBinding>(
+            LayoutInflater.from(viewGroup.context),
+            R.layout.item_cryptocurrency,
+            viewGroup,
+            false
+        )
     )
 
     override fun getItemCount() = viewModel.data.size
 
     override fun onBindViewHolder(viewHolder: MainViewHolder, value: Int) {
-        val source = viewModel.data[value]
-
-        viewHolder.apply {
-            number.text = source.number
-            name.text = source.name
-            symbol.text = source.symbol
-        }
+        viewHolder.bind(viewModel.data[value])
     }
 
-    class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val number = itemView.findViewById<TextView>(R.id.number)!!
-        val name = itemView.findViewById<TextView>(R.id.name)!!
-        val symbol = itemView.findViewById<TextView>(R.id.symbol)!!
+    class MainViewHolder(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Cryptocurrency) {
+            binding.setVariable(BR.cryptocurrency, data)
+            binding.executePendingBindings()
+        }
     }
 }
