f7c086d9f635b19a41a0f22863462fa4cfcc048c
Nourepide <nourepide@gmail.com>
2018-11-01 13:55:03 +0400

    Reformat MainListAdapter

--- app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
+++ app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
@@ -5,17 +5,18 @@ import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import net.nourepide.learning.cryptocurrencymonitor.MainListAdapter.MainViewHolder
 import net.nourepide.learning.cryptocurrencymonitor.entity.Cryptocurrency
 
-class MainListAdapter(private val viewModel: MainViewModel) : RecyclerView.Adapter<MainListAdapter.MainViewHolder>() {
-    override fun onCreateViewHolder(viewGroup: ViewGroup, value: Int) = MainViewHolder(
-        DataBindingUtil.inflate<ViewDataBinding>(
-            LayoutInflater.from(viewGroup.context),
-            R.layout.item_cryptocurrency,
-            viewGroup,
-            false
-        )
-    )
+class MainListAdapter(private val viewModel: MainViewModel) : RecyclerView.Adapter<MainViewHolder>() {
+    class MainViewHolder(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Cryptocurrency) {
+            binding.setVariable(BR.cryptocurrency, data)
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, value: Int) = MainViewHolder(getLayout(viewGroup))
 
     override fun getItemCount() = viewModel.data.size
 
@@ -23,10 +24,11 @@ class MainListAdapter(private val viewModel: MainViewModel) : RecyclerView.Adapt
         viewHolder.bind(viewModel.data[value])
     }
 
-    class MainViewHolder(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(data: Cryptocurrency) {
-            binding.setVariable(BR.cryptocurrency, data)
-            binding.executePendingBindings()
-        }
-    }
+    private fun getLayout(viewGroup: ViewGroup): ViewDataBinding = DataBindingUtil
+        .inflate(
+            LayoutInflater.from(viewGroup.context),
+            R.layout.item_cryptocurrency,
+            viewGroup,
+            false
+        )
 }
