4ade3cbf9d982fa85f8f8047f4fa2fc8f08a18ad
Nourepide <nourepide@gmail.com>
2018-11-05 12:47:50 +0400

    Improve reload

--- app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainActivity.kt
+++ app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainActivity.kt
@@ -3,6 +3,7 @@ package net.nourepide.learning.cryptocurrencymonitor
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.annotation.MainThread
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
@@ -12,30 +13,34 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by lazy { ViewModelProviders.of(this).get(MainViewModel::class.java) }
-    private val binding by lazy {
-        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-    }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.list.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = MainListAdapter(viewModel)
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         thread {
             viewModel.initialization()
-
             runOnUiThread { reload() }
         }
     }
 
+    @MainThread
     private fun reload() {
-        binding.apply {
-            progressBar.visibility = View.GONE
-            list.adapter!!.notifyDataSetChanged()
-            list.startAnimation(AnimationUtils.loadAnimation(this.root.context, R.anim.appearance))
+        fun disableProgressBar() {
+            binding.progressBar.visibility = View.GONE
         }
+
+        fun setDataRecyclerView() {
+            binding.recyclerView.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = MainListAdapter(viewModel).apply { notifyDataSetChanged() }
+                startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.appearance))
+            }
+        }
+
+        disableProgressBar()
+        setDataRecyclerView()
     }
 }
