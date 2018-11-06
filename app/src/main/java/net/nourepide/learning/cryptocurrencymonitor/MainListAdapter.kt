056758107293e14cf8b78a5805e588303edf18b6
Nourepide <nourepide@gmail.com>
2018-11-06 13:44:20 +0400

    Fix arguments in MainListAdapter

--- app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
+++ app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
@@ -1,6 +1,5 @@
 package net.nourepide.learning.cryptocurrencymonitor
 
-import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -8,11 +7,11 @@ import net.nourepide.learning.cryptocurrencymonitor.MainListAdapter.MainViewHold
 import net.nourepide.learning.cryptocurrencymonitor.databinding.ItemCryptocurrencyBinding
 
 class MainListAdapter(private val viewModel: MainViewModel) : RecyclerView.Adapter<MainViewHolder>() {
-    override fun onCreateViewHolder(viewGroup: ViewGroup, value: Int) = MainViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MainViewHolder(
         ItemCryptocurrencyBinding
             .inflate(
-                LayoutInflater.from(viewGroup.context),
-                viewGroup,
+                LayoutInflater.from(parent.context),
+                parent,
                 false
             )
     )
@@ -20,8 +19,8 @@ class MainListAdapter(private val viewModel: MainViewModel) : RecyclerView.Adapt
     override fun getItemCount() = viewModel.data.size
 
     override fun onBindViewHolder(viewHolder: MainViewHolder, value: Int) {
-        (viewHolder.binding as ItemCryptocurrencyBinding).cryptocurrency = viewModel.data[value]
+        viewHolder.binding.cryptocurrency = viewModel.data[value]
     }
 
-    class MainViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+    class MainViewHolder(val binding: ItemCryptocurrencyBinding) : RecyclerView.ViewHolder(binding.root)
 }
