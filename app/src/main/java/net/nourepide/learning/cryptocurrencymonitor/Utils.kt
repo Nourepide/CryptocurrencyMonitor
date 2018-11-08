06fc972b574c8d0239479aa9553e65f02aef38cf
Nourepide <nourepide@gmail.com>
2018-11-08 16:49:46 +0400

    Add mutableLiveData to Utils

--- app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/Utils.kt
+++ app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/Utils.kt
@@ -1,5 +1,6 @@
 package net.nourepide.learning.cryptocurrencymonitor
 
+import android.arch.lifecycle.MutableLiveData
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -30,6 +31,16 @@ class Utils {
             }
 
             return buffer.toString()
+        }
+
+        fun <T> mutableLiveData(defaultValue: T? = null): MutableLiveData<T> {
+            val data = MutableLiveData<T>()
+
+            if (defaultValue != null) {
+                data.value = defaultValue
+            }
+
+            return data
         }
     }
 }
