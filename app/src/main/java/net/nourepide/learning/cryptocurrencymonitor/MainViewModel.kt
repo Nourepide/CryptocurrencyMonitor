f0e11d125c95dfce0c6c5fe215196b0ce68d5ca2
Nourepide <nourepide@gmail.com>
2018-11-08 16:54:18 +0400

    Set MutableLiveData as function

--- app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainViewModel.kt
+++ app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainViewModel.kt
@@ -1,34 +1,30 @@
 package net.nourepide.learning.cryptocurrencymonitor
 
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import net.nourepide.learning.cryptocurrencymonitor.Utils.Companion.mutableLiveData
 import net.nourepide.learning.cryptocurrencymonitor.entity.Cryptocurrency
 import org.json.JSONObject
 import kotlin.concurrent.thread
 
 class MainViewModel : ViewModel() {
-    val data = MutableLiveData<List<Cryptocurrency>>()
-    var isLoading = MutableLiveData<Boolean>()
+    val data = mutableLiveData(listOf<Cryptocurrency>())
+    var isLoading = mutableLiveData(false)
 
     init {
         thread {
-            initialization()
+            val jsonArray = JSONObject(Utils.getDataURL()).getJSONArray("data")
+
+            (0 until jsonArray.length())
+                .map { jsonArray[it] as JSONObject }
+                .map {
+                    Cryptocurrency(
+                        it.getString("id"),
+                        it.getString("name"),
+                        it.getString("symbol")
+                    )
+                }.toList().apply { data.postValue(this) }
+
+            isLoading.postValue(false)
         }
-    }
-
-    fun initialization() {
-        val jsonArray = JSONObject(Utils.getDataURL()).getJSONArray("data")
-
-        (0 until jsonArray.length())
-            .map { jsonArray[it] as JSONObject }
-            .map {
-                Cryptocurrency(
-                    it.getString("id"),
-                    it.getString("name"),
-                    it.getString("symbol")
-                )
-            }.toList().apply { data.postValue(this) }
-
-        isLoading.postValue(false)
     }
 }
