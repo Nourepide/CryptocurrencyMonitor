dde2f773840fa16d4bf12b12cb07e58cb310eb9b
Nourepide <nourepide@gmail.com>
2018-11-15 23:42:36 +0400

    Set DialogFragment in listener

--- app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
+++ app/src/main/java/net/nourepide/learning/cryptocurrencymonitor/MainListAdapter.kt
@@ -2,6 +2,7 @@ package net.nourepide.learning.cryptocurrencymonitor
 
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.Observer
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.RecyclerView.Adapter
 import android.view.LayoutInflater
@@ -9,23 +10,25 @@ import android.view.ViewGroup
 import net.nourepide.learning.cryptocurrencymonitor.MainListAdapter.MainViewHolder
 import net.nourepide.learning.cryptocurrencymonitor.databinding.ItemCryptocurrencyBinding
 
-class MainListAdapter(lifecycle: LifecycleOwner, private val viewModel: MainViewModel) : Adapter<MainViewHolder>() {
+class MainListAdapter(val lifecycle: LifecycleOwner, private val viewModel: MainViewModel) : Adapter<MainViewHolder>() {
 
     init {
         viewModel.data.observe(lifecycle, Observer { notifyDataSetChanged() })
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-        return MainViewHolder(
-            ItemCryptocurrencyBinding
-                .inflate(LayoutInflater.from(parent.context), parent, false)
-        )
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MainViewHolder(
+        ItemCryptocurrencyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
 
     override fun getItemCount() = viewModel.data.value!!.size
 
     override fun onBindViewHolder(viewHolder: MainViewHolder, value: Int) {
         viewHolder.binding.cryptocurrency = viewModel.data.value!![value]
+        viewHolder.itemView.setOnClickListener {
+            MainDialogFragment()
+                .setArguments("TITLE", viewHolder.binding.cryptocurrency!!.run { "$name : $symbol" })
+                .show((lifecycle as AppCompatActivity).supportFragmentManager, "tag")
+        }
     }
 
     class MainViewHolder(val binding: ItemCryptocurrencyBinding) : RecyclerView.ViewHolder(binding.root)
